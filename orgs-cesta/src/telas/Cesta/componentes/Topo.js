277fdaf5c81bc945a519c59576b2eb8cad9905f1
Larii04 <larissagfandrade@gmail.com>
2023-03-01 18:22:48 -0300

    conteúdo do app
    Contém todo o desenvolvimento do app.

---
+++ orgs-cesta/src/telas/Cesta/componentes/Topo.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Image, StyleSheet, Dimensions} from "react-native";
+
+import Texto from "../../../componentes/Texto";
+import topo from '../../../../assets/topo.png';
+
+const width = Dimensions.get('screen').width; //tamanho da tela com Dimensions (largura)
+
+export default function Topo({titulo}){
+	return <>
+		<Image source={topo} style={estilos.topo} />
+		<Texto style={estilos.titulo}>{titulo}</Texto>
+	</>
+}
+
+const estilos = StyleSheet.create({
+	topo:{
+		width: "100%",
+		height: 578 / 768 * width,
+	},
+
+	titulo:{
+		width: "100%",
+		position: "absolute",
+		textAlign: "center",
+		fontSize: 20,
+		lineHeight: 26, 
+		color: "white",
+		fontWeight: "bold",
+		padding: 16,
+		marginTop: 40,
+	},
+})
